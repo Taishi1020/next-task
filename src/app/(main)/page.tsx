6ca9c169a0662dde94b740f5e578734e478b76fc
Taishi1020 <tai.yakiman1020@gmail.com>
2024-07-22 22:00:04 +0900

    [add] ErrorPageの実装

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { MdAddTask } from "react-icons/md";
 
 export default function MainPage() {
+  
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
